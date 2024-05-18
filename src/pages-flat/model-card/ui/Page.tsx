08c748ba710f144509a4cf1c0d08714b8aef75c0
Mikhail Barulin <barulin.ma@gmail.com>
2024-05-18 20:51:30 +0300

    fix: Адаптив

--- src/pages-flat/model-card/ui/Page.tsx
+++ src/pages-flat/model-card/ui/Page.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import { Tabs, Breadcrumb } from "@/shared/components";
+import { useState } from 'react';
+import { Tabs, Breadcrumb } from '@/shared/components';
 import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/shared/components/Breadcrumb";
-import { CommonLayout } from "@/widgets/common-layout";
-import { CardHead } from "./CardHead/CardHead";
-import { DescriptionTab } from "./tabs/DescriptionTab";
-import { PlaygroundTab } from "./tabs/PlaygroundTab";
-import { ExamplesTab } from "./tabs/ExamplesTab";
-import { InferGetServerSidePropsType } from "next";
-import { getServerSideProps } from "../ssr";
+} from '@/shared/components/Breadcrumb';
+import { CommonLayout } from '@/widgets/common-layout';
+import { CardHead } from './CardHead/CardHead';
+import { DescriptionTab } from './tabs/DescriptionTab';
+import { PlaygroundTab } from './tabs/PlaygroundTab';
+import { ExamplesTab } from './tabs/ExamplesTab';
+import { InferGetServerSidePropsType } from 'next';
+import { getServerSideProps } from '../ssr';
 
 type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 export function Page({ model }: PageProps) {
   const { info, reviews, similar, examples, description } = model;
-  const [activeTab, setActiveTab] = useState<string>("playground");
+  const [activeTab, setActiveTab] = useState<string>('playground');
 
   return (
     <CommonLayout>
@@ -39,14 +39,14 @@ export function Page({ model }: PageProps) {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-        <div className="w-full flex align-start flex-col-reverse md:flex-row md:justify-between mt-5">
+        <div className="align-start mt-5 flex w-full flex-col-reverse md:flex-row md:justify-between">
           <CardHead info={model.info} />
         </div>
         <Tabs
           activeTab={activeTab}
           setActiveTab={setActiveTab}
           type="underline"
-          className="mt-8 ml-[-8px] w-full overflow-hidden"
+          className="ml-[-8px] mt-8 w-full justify-start overflow-hidden xl:ml-0"
         >
           <Tabs.TabItem className="text-lg" tabKey="playground">
             Использовать
@@ -61,24 +61,32 @@ export function Page({ model }: PageProps) {
               Примеры
             </Tabs.TabItem>
           )}
-          {reviews && <Tabs.TabItem className="text-lg" tabKey="reviews">
-            Отзывы
-          </Tabs.TabItem>}
-          {similar && <Tabs.TabItem className="text-lg" tabKey="alternatives">
-            Похожее
-          </Tabs.TabItem>}
+          {reviews && (
+            <Tabs.TabItem className="text-lg" tabKey="reviews">
+              Отзывы
+            </Tabs.TabItem>
+          )}
+          {similar && (
+            <Tabs.TabItem className="text-lg" tabKey="alternatives">
+              Похожее
+            </Tabs.TabItem>
+          )}
         </Tabs>
       </div>
-      <div className="w-full mt-6 mb-8">
+      <div className="mb-8 mt-6 w-full">
         <Tabs.TabContent tabKey="playground" activeTab={activeTab}>
           <PlaygroundTab />
         </Tabs.TabContent>
-        {description && <Tabs.TabContent tabKey="description" activeTab={activeTab}>
-          <DescriptionTab />
-        </Tabs.TabContent>}
-        {examples && <Tabs.TabContent tabKey="examples" activeTab={activeTab}>
-          <ExamplesTab />
-        </Tabs.TabContent>}
+        {description && (
+          <Tabs.TabContent tabKey="description" activeTab={activeTab}>
+            <DescriptionTab />
+          </Tabs.TabContent>
+        )}
+        {examples && (
+          <Tabs.TabContent tabKey="examples" activeTab={activeTab}>
+            <ExamplesTab />
+          </Tabs.TabContent>
+        )}
       </div>
     </CommonLayout>
   );
